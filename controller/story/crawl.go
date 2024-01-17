4364270a4fe1e07360a3cfaaf302fcc3d16d965e
lekhoan <khoanld98@gmail.com>
2024-01-17 23:47:54 +0700

    docker file

--- controller/story/crawl.go
+++ controller/story/crawl.go
@@ -9,29 +9,23 @@ import (
 
 func Crawl(ctx *gin.Context) {
 	crawler := colly.NewCollector()
-	ctx.JSON(200, gin.H{
-		"message": 123,
-	})
-	err := crawler.Visit("https://dtruyen.com/thap-nien-60-xuyen-thanh-nong-nu-hai-thuoc-lam-giau/chuong-2_5124424.html")
-	fmt.Printf("======================Visiting: %+v======================", err)
-	fmt.Printf("======================Visiting: %+v======================", crawler)
 	crawler.OnRequest(func(r *colly.Request) {
-		fmt.Printf("======================Visiting: %+v======================", r.URL)
+		fmt.Println("Visiting\n", r.URL)
 	})
 
 	// crawler.OnError(func(_ *colly.Response, err error) {
 	// 	fmt.Printf("Something went wrong: ", err)
 	// })
 
-	// crawler.OnResponse(func(r *colly.Response) {
-	// 	fmt.Printf("Page visited: ", r.Request.URL)
-	// })
+	ctx.JSON(200, gin.H{
+		"message": 123,
+	})
 
-	// crawler.OnHTML("a", func(e *colly.HTMLElement) {
-	// 	// printing all URLs associated with the a links in the page
-	// 	fmt.Printf("%+v", e)
-	// })
-
+	crawler.OnHTML("#chapter-content", func(e *colly.HTMLElement) {
+		// printing all URLs associated with the a links in the page
+		fmt.Printf("===%+v---\n", e.Text)
+	})
+	crawler.Visit("https://dtruyen.com/thap-nien-60-xuyen-thanh-nong-nu-hai-thuoc-lam-giau/chuong-1_5124423.html")
 	// crawler.OnScraped(func(r *colly.Response) {
 	// 	fmt.Println(r.Request.URL, " scraped!")
 	// })

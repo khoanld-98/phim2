00270d43180265d29eb6c33072682c8a1caf873f
lekhoan <khoanld98@gmail.com>
2024-01-29 00:23:37 +0700

    foreach

--- service/crawl/crawStoryService.go
+++ service/crawl/crawStoryService.go
@@ -2,6 +2,7 @@ package crawl
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gocolly/colly"
@@ -30,6 +31,17 @@ func (cs *CrawlStoryService) prepareCrawl(page int) {
 	crawl := colly.NewCollector()
 
 	crawl.OnHTML(cs.Query.Auth, func(e *colly.HTMLElement) {
-		fmt.Printf("---------------%+v------------------", e.Text)
+		html, _ := e.DOM.Html()
+
+		fmt.Printf("%+v", html)
+		e.ForEach("div", func(_ int, el *colly.HTMLElement) {
+			text := strings.TrimSpace(e.Text)
+			fmt.Println(text)
+		})
+		// e.ForEach("div", func(i int, h *colly.HTMLElement) {
+		// 	fmt.Printf("%+v: %+v", i, h.Text)
+		// })
 	})
+
+	crawl.Visit(cs.Query.UrlVisit)
 }
